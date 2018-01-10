b0b322892fffd8cfdc782c6337cb0502828e036f
OEM Configuration (temporary user) <davidndy@gmail.com>
2018-01-09 19:59:04 -0500

    experience section done

--- js/app.js
+++ js/app.js
@@ -4,4 +4,14 @@ $( document ).ready(function() {
 		console.log("nav bar")
 		$(this).addClass('active');
 	})
+
+	$(".timeline-item a").on("click",function(e){
+		var destiny = $(this).attr("href");
+		$(".tabs-content > div").each(function(){
+			$(this).removeClass('active')
+			$(`.tabs-content ${destiny}`).addClass('active')
+		})
+	})
+
+	
 });
