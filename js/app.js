b9267796787b1837ff4719bab425262801942297
David Mejia <davidndy@gmail.com>
2018-02-11 19:26:18 -0500

    Skills and scroll down option done

--- js/app.js
+++ js/app.js
@@ -14,5 +14,51 @@ $( document ).ready(function() {
 			$(`.tabs-content ${destiny}`).addClass('active')
 		})
 	})
+	
+	var sections = $('section')
+	  , nav = $('nav')
+	  , nav_height = nav.outerHeight();
+	 
+	$(window).on('scroll', function () {
+	  var cur_pos = $(this).scrollTop();
+	 
+	  sections.each(function() {
+	    var top = $(this).offset().top - nav_height,
+	        bottom = top + $(this).outerHeight();
+	 
+	    if (cur_pos >= top && cur_pos <= bottom) {
+	      nav.find('a').removeClass('active');
+	      sections.removeClass('active');
+	 
+	      $(this).addClass('active');
+	      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+	    }
+	  });
+	});
 
+	//Skills item appear disappear section.
+    $( ".item1" ).hover(
+  function() {
+  	console.log(this)
+    $(this).children("h5").css("color","#777777");
+  }, function() {
+    $(".item1 h5").css("color","white");
+  		}
+	);
+	 $( ".item2" ).hover(
+  function() {
+  	console.log(this)
+    $(this).children("h5").css("color","#777777");
+  }, function() {
+    $(".item2 h5").css("color","white");
+  		}
+	);
+	  $( ".item3" ).hover(
+  function() {
+  	console.log(this)
+    $(this).children("h5").css("color","#777777");
+  }, function() {
+    $(".item3 h5").css("color","white");
+  		}
+	);
 });
